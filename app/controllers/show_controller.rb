8158fc6709e3aa15650c5316ef6755dd6657506a
Jared <mailing-list@blitzstein.net>
2010-06-09 20:47:19 -0400

    Updated boolean statement

--- app/controllers/show_controller.rb
+++ app/controllers/show_controller.rb
@@ -9,15 +9,15 @@ class ShowController < ApplicationController
     #render :action => "jobs_temp"
     @administrative = Category.find(1)
     logger.debug("Found Category #{@administrative.name}")
-    @administrative.jobs = Job.find(:all, :conditions => "category_id = #{@administrative.id} and ((start_date <= current_date and end_date >= current_date) or (start_date is null and end_date >= current_date) or (start_date <= current_date and end_date = null) or (start_date is null and end_date is null))", :order=>"position")
+    @administrative.jobs = Job.find(:all, :conditions => "category_id = #{@administrative.id} and is_active = 't' and ((start_date <= current_date and end_date >= current_date) or (start_date is null and end_date >= current_date) or (start_date <= current_date and end_date = null) or (start_date is null and end_date is null))", :order=>"position")
   
     @industrial = Category.find(2)
     logger.debug("Found Category #{@industrial.name}")
-    @industrial.jobs = Job.find(:all, :conditions => "category_id = #{@industrial.id} and ((start_date <= current_date and end_date >= current_date) or (start_date is null and end_date >= current_date) or (start_date <= current_date and end_date = null) or (start_date is null and end_date is null))", :order=>"position")
+    @industrial.jobs = Job.find(:all, :conditions => "category_id = #{@industrial.id} and is_active = 't' and ((start_date <= current_date and end_date >= current_date) or (start_date is null and end_date >= current_date) or (start_date <= current_date and end_date = null) or (start_date is null and end_date is null))", :order=>"position")
   
     @professional = Category.find(3)
     logger.debug("Found Category #{@professional.name}")
-    @professional.jobs = Job.find(:all, :conditions => "category_id = #{@professional.id} and ((start_date <= current_date and end_date >= current_date) or (start_date is null and end_date >= current_date) or (start_date <= current_date and end_date = null) or (start_date is null and end_date is null))", :order=>"position")
+    @professional.jobs = Job.find(:all, :conditions => "category_id = #{@professional.id} and is_active = 't' and ((start_date <= current_date and end_date >= current_date) or (start_date is null and end_date >= current_date) or (start_date <= current_date and end_date = null) or (start_date is null and end_date is null))", :order=>"position")
   
   end
   
